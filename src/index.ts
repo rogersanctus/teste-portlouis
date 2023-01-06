af8fb7ef8b782c253bc52fc427237f604ef89708
rogersanctus <rogersanctus@gmail.com>
2023-01-06 16:27:56 -0300

    feat: implements the data crossing of Notas and Pedidos generating a list with their relationship and the sums of quantidade_produto of each NotaItem that is related with the número_item of a Produto inside the related Pedido

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,74 @@
 import { initDB } from './utils/dbReader'
-import { IRecord } from './models/record'
-import { IRecordUser } from './models/record_user'
-import { IUser } from './models/user'
-import groupBy from 'lodash/groupBy'
+import { FinalPedido } from './models/pedido'
+import { FinalNota, NotaItem } from './models/nota'
+import { processDB } from './utils/dbProcessor'
+import cloneDeep from 'lodash/cloneDeep'
 
 initDB('./assets/db').then((db) => {
-  const users = db.Users as IUser[]
-  const records = db.Records as IRecord[]
+  db = processDB(db)
+  const pedidos = db.Pedidos as FinalPedido[]
+  const notas = db.Notas as FinalNota[]
 
-  const groupedRecords = groupBy(records, 'id')
+  const pedidosNotas = pedidos.map((pedido) => {
+    const notasPedido: FinalNota[] = []
 
-  const recordsUsers = Object.keys(groupedRecords).map<IRecordUser>(
-    (groupedRecordKey) => {
-      const records = groupedRecords[groupedRecordKey]
-      const initialRecordUser: IRecordUser = {
-        record_id: -1,
-        users: [],
+    notas.forEach((nota) => {
+      const newItems = nota.items.filter(
+        (notaItem) => notaItem.id_pedido === pedido.id
+      )
+
+      if (newItems.length > 0) {
+        notasPedido.push({ ...nota, items: newItems })
       }
+    })
 
-      return records.reduce<IRecordUser>((accum, record, idx) => {
-        if (idx === 0) {
-          accum.record_id = record.id
-        }
-
-        const user = users.find((user) => user.id === record.user_id)
-
-        if (user) {
-          accum.users.push(user)
-        }
-
-        return accum
-      }, initialRecordUser)
+    return {
+      pedido,
+      notas: notasPedido,
     }
-  )
+  })
 
-  console.dir(recordsUsers, { depth: Infinity })
+  const totaisPedidosNotas = pedidosNotas.map((pedidoNota) => {
+    const notasCopy = cloneDeep(pedidoNota.notas)
+    const sameItems: { [key: number]: NotaItem & { itemsNota?: NotaItem[] } } =
+      {}
+
+    const notas = notasCopy.reduce((accum, nota) => {
+      nota.items.forEach((notaItem) => {
+        accum[notaItem.número_item] = {
+          id_pedido: notaItem.id_pedido,
+          número_item: notaItem.número_item,
+          quantidade_produto: notaItem.quantidade_produto,
+          itemsNota: [notaItem],
+        }
+
+        notasCopy.forEach((otherNota, otherNotaIdx) => {
+          if (otherNota !== nota) {
+            otherNota.items.forEach((otherNotaItem, idx) => {
+              if (notaItem.número_item === otherNotaItem.número_item) {
+                accum[notaItem.número_item].quantidade_produto +=
+                  otherNotaItem.quantidade_produto
+                accum[notaItem.número_item].itemsNota?.push(otherNotaItem)
+
+                otherNota.items.splice(idx, 1)
+
+                if (otherNota.items.length === 0) {
+                  notasCopy.splice(otherNotaIdx, 1)
+                }
+              }
+            })
+          }
+        })
+      })
+
+      return accum
+    }, sameItems)
+
+    return {
+      ...pedidoNota,
+      notas,
+    }
+  })
+
+  console.dir(totaisPedidosNotas, { depth: Infinity })
 })
