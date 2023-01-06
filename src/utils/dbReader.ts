7361399d7c2a1278513877bbca5d0713bc7a15ad
rogersanctus <rogersanctus@gmail.com>
2023-01-06 16:16:06 -0300

    feat: makes it possible to read UTF-8 files adding the ability to remove the BOM signature from the read file

--- src/utils/dbReader.ts
+++ src/utils/dbReader.ts
@@ -1,8 +1,27 @@
 import { readFile, opendir } from 'node:fs/promises'
 import { resolve } from 'node:path'
 
+export type DB = Record<string, unknown[] | Record<string, unknown>>
+
 export async function initDB(path: string) {
-  const db: Record<string, unknown[]> = {}
+  const db: DB = {}
+
+  async function readUTF8(filename: string) {
+    const buffer = await readFile(filename)
+
+    const rawData = new Uint8Array(buffer)
+    const data = removeBOM(rawData)
+    const decoder = new TextDecoder('utf-8')
+    return decoder.decode(data)
+  }
+
+  function removeBOM(data: Uint8Array) {
+    if (data[0] === 0xef && data[1] === 0xbb && data[2] === 0xbf) {
+      return data.subarray(3)
+    }
+
+    return data
+  }
 
   async function readDB(path: string) {
     const dir = await opendir(path)
@@ -16,7 +35,7 @@ export async function initDB(path: string) {
         for await (const fileDirent of await opendir(dbDirPath)) {
           if (fileDirent.isFile()) {
             const dbFilePath = resolve(dbDirPath, fileDirent.name)
-            const content = await readFile(dbFilePath, { encoding: 'utf-8' })
+            const content = await readUTF8(dbFilePath)
 
             try {
               const contentLines = content
@@ -26,7 +45,7 @@ export async function initDB(path: string) {
               for (const line of contentLines) {
                 const jobject = JSON.parse(line)
                 entry.push({
-                  id: Number(fileDirent.name.split(/^[a-z]+|\..+$/)[1]),
+                  id: Number(fileDirent.name.split(/^[a-z]+|\..+$/i)[1]),
                   ...jobject,
                 })
               }
