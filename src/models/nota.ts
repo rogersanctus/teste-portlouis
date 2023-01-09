45ea2a05b868f718124b6c7ec8b759ab152dafac
rogersanctus <rogersanctus@gmail.com>
2023-01-09 19:23:00 -0300

    feat: adds validation for the fields of Notas and Pedidos. An error will be thrown when a field is not valid

--- src/models/nota.ts
+++ src/models/nota.ts
@@ -1,3 +1,5 @@
+import { isInteger, isPositive, isValidNumber } from '../utils/dataValidators'
+
 export interface NotaItem {
   id_pedido: number
   número_item: number
@@ -13,6 +15,44 @@ export interface FinalNota {
   items: NotaItem[]
 }
 
+function validateNota(nota: Nota) {
+  if (!isValidNumber(nota.id_pedido)) {
+    return new Error(
+      `The id_pedido the nota with id: ${nota.id} is not a valid number`
+    )
+  }
+
+  if (
+    !isValidNumber(nota.número_item) ||
+    !isInteger(nota.número_item) ||
+    nota.número_item < 0
+  ) {
+    return new Error(
+      `The número_item of the nota with id ${nota.id} is not a valid positive and integer number`
+    )
+  }
+
+  if (
+    !isValidNumber(nota.quantidade_produto) ||
+    !isInteger(nota.quantidade_produto) ||
+    !isPositive(nota.quantidade_produto)
+  ) {
+    return new Error(
+      `The quantidade_produto of the nota with id ${nota.id} is not a valid positive and integer number`
+    )
+  }
+
+  return true
+}
+
 export function processNotas(rawNotas: Nota[]) {
-  return [...rawNotas]
+  return rawNotas.map((nota) => {
+    const error = validateNota(nota)
+
+    if (error instanceof Error) {
+      throw error
+    }
+
+    return nota
+  })
 }
--- src/models/pedido.ts
+++ src/models/pedido.ts
@@ -1,22 +1,75 @@
+import {
+  isAlphaNumeric,
+  isFractionLike,
+  isInteger,
+  isPositive,
+  isValidNumber,
+} from '../utils/dataValidators'
 import { Produto, RawProduto } from './produto'
 
 export interface Pedido extends Produto {
   id: number
 }
 
-interface RawPedido extends RawProduto {
+export interface RawPedido extends RawProduto {
   id: number
+}
+
+function validatePedido(pedido: RawPedido) {
+  if (
+    !isValidNumber(pedido.número_item) ||
+    !isInteger(pedido.número_item) ||
+    pedido.número_item < 0
+  ) {
+    return new Error(
+      `The número_item of the pedido with id ${pedido.id} is not a valid positive and integer number`
+    )
+  }
+
+  if (!isAlphaNumeric(pedido.código_produto)) {
+    return new Error(
+      `The código_produto of the pedido with id ${pedido.id} is not a valid alphanumeric value`
+    )
+  }
+
+  if (
+    !isValidNumber(pedido.quantidade_produto) ||
+    !isInteger(pedido.quantidade_produto) ||
+    !isPositive(pedido.quantidade_produto)
+  ) {
+    return new Error(
+      `The quantidade_produto of the pedido with id ${pedido.id} is not a valid positive and integer number`
+    )
+  }
+
+  if (!isFractionLike(pedido.valor_unitário_produto)) {
+    return new Error(
+      `The valor_unitário_produto of the pedido with id ${pedido.id} is not a valid decimal value`
+    )
+  }
+
+  const valorUnitario = Number(pedido.valor_unitário_produto.replace(',', '.'))
+
+  if (isNaN(valorUnitario) || !isPositive(valorUnitario)) {
+    return new Error(
+      `The valor_unitário_produto of the pedido with id ${pedido.id} is not a valid positive number`
+    )
+  }
+
+  return { ...pedido, valorUnitario }
 }
 
 export function processPedidos(rawPedidos: RawPedido[]) {
   return rawPedidos.map((rawPedido) => {
-    const valorUnitario = Number(
-      rawPedido.valor_unitário_produto.replace(',', '.')
-    )
+    const result = validatePedido(rawPedido)
+
+    if (result instanceof Error) {
+      throw result
+    }
 
     return {
       ...rawPedido,
-      valor_unitário_produto: valorUnitario,
+      valor_unitário_produto: result.valorUnitario,
     } as Pedido
   })
 }
