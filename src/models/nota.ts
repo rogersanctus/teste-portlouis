62ef37d46574a1bb2a3731679772a491532995d9
rogersanctus <rogersanctus@gmail.com>
2023-01-09 21:31:49 -0300

    feat: implements a validation to check if the item in the Nota exists in the refered Pedido

--- src/models/nota.ts
+++ src/models/nota.ts
@@ -1,4 +1,5 @@
 import { isInteger, isPositive, isValidNumber } from '../utils/dataValidators'
+import { Pedido } from './pedido'
 
 export interface NotaItem {
   id_pedido: number
@@ -40,6 +41,24 @@ function validateNota(nota: Nota) {
     return new Error(
       `The quantidade_produto of the nota with id ${nota.id} is not a valid positive and integer number`
     )
+  }
+
+  return true
+}
+
+export function validateNotasItems(notas: Nota[], pedidos: Pedido[]) {
+  for (const notaItem of notas) {
+    const pedidoItem = pedidos.find(
+      (pedido) =>
+        pedido.id === notaItem.id_pedido &&
+        pedido.número_item === notaItem.número_item
+    )
+
+    if (pedidoItem === undefined) {
+      return new Error(
+        `The Item, of the Nota with id: ${notaItem.id}, with the número_item: ${notaItem.número_item} was not found in the refered Pedido with id_pedido: ${notaItem.id_pedido}`
+      )
+    }
   }
 
   return true
