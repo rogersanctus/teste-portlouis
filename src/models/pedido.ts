9f3ed7159185a7bf250473921624c2ec24dfe203
rogersanctus <rogersanctus@gmail.com>
2023-01-09 21:30:08 -0300

    feat: implements the general validation of Pedidos. Throwing errors if an item of pedido is duplicated or if all the items inside de sequênce between the lesser número_item and the greater número_item do not exists.

--- src/models/pedido.ts
+++ src/models/pedido.ts
@@ -59,6 +59,75 @@ function validatePedido(pedido: RawPedido) {
   return { ...pedido, valorUnitario }
 }
 
+/**
+ *
+ * @param items is a subset of Pedidos where all their Pedido id is the same
+ */
+export function validateItemsPedido(items: Pedido[]) {
+  const id_pedido = items[0].id
+  const firstNumeroItem = items[0].número_item
+  let minNumeroItem = firstNumeroItem,
+    maxNumeroItem = firstNumeroItem
+
+  const duplicatedCheckingList = [...items].map((checkingItem, idx) => ({
+    idx,
+    ...checkingItem,
+  }))
+  let hasDuplicated = false
+
+  items.forEach((item, idx) => {
+    if (item.número_item > maxNumeroItem) maxNumeroItem = item.número_item
+    if (item.número_item < minNumeroItem) minNumeroItem = item.número_item
+
+    if (
+      duplicatedCheckingList.find(
+        (checkingItem) =>
+          idx !== checkingItem.idx &&
+          checkingItem.número_item === item.número_item
+      )
+    ) {
+      hasDuplicated = true
+      duplicatedCheckingList.splice(0, duplicatedCheckingList.length)
+    } else {
+      const toDeleteIdx = duplicatedCheckingList.findIndex(
+        (checkingItem) => checkingItem.idx === idx
+      )
+
+      if (toDeleteIdx !== -1) {
+        duplicatedCheckingList.splice(toDeleteIdx, 1)
+      }
+    }
+  })
+
+  let haveAll = true
+  const copy = [...items]
+
+  for (let num = minNumeroItem; num <= maxNumeroItem; num++) {
+    const found = copy.findIndex((item) => item.número_item === num)
+
+    if (found === -1) {
+      haveAll = false
+      break
+    }
+
+    copy.splice(found, 1)
+  }
+
+  if (!haveAll) {
+    return new Error(
+      `The Pedido with id: ${id_pedido} has not all the número_item inside de sequence between ${minNumeroItem} and ${maxNumeroItem}.`
+    )
+  }
+
+  if (hasDuplicated) {
+    return new Error(
+      `The Pedido with id: ${id_pedido} has items with duplicated número_item.`
+    )
+  }
+
+  return true
+}
+
 export function processPedidos(rawPedidos: RawPedido[]) {
   return rawPedidos.map((rawPedido) => {
     const result = validatePedido(rawPedido)
