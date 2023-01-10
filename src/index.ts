a14c70e8621787a517e33fb9b0e549df13120cd0
rogersanctus <rogersanctus@gmail.com>
2023-01-10 12:35:37 -0300

    feat: formats the pedidos pendentes into pt-BR localized objects representation for currency values feat: write the PedidosPedentes file

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,11 @@ import groupBy from 'lodash/groupBy'
 import { processDB } from './utils/dbProcessor'
 import { makeNotasPedidos } from './services/notasPedidos'
 import { makePedidosPendentes } from './services/pedidosPendentes'
+import { writeToDB } from './utils/dbWriter'
 
-initDB('./assets/db').then((db) => {
+const DB_PATH = './assets/db'
+
+initDB(DB_PATH).then((db) => {
   db = processDB(db)
   const pedidos = db.Pedidos as Pedido[]
   const notas = db.Notas as Nota[]
@@ -20,6 +23,31 @@ initDB('./assets/db').then((db) => {
   const groupedPedidos = groupBy(pedidos, 'id')
   const notasPedidos = makeNotasPedidos(groupedPedidos, notas)
   const pedidosPendentes = makePedidosPendentes(notasPedidos)
+  const pedidosPendentesFormatted = pedidosPendentes.map(
+    (itemPedidoPendente) => {
+      const formatter = Intl.NumberFormat('pt-BR', {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+        useGrouping: false,
+      })
+
+      return {
+        ...itemPedidoPendente,
+        valor_total_pedido: formatter.format(
+          itemPedidoPendente.valor_total_pedido
+        ),
+        valor_total_pendente: formatter.format(
+          itemPedidoPendente.valor_total_pendente
+        ),
+      }
+    }
+  )
 
   console.dir(pedidosPendentes, { depth: Infinity })
+  writeToDB(
+    DB_PATH,
+    'PedidosPendentes',
+    'Itens_Pendentes.txt',
+    pedidosPendentesFormatted
+  )
 })
