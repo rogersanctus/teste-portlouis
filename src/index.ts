aa9ebbacd9bb3dcf2216c0ce0c261883b9f512ab
rogersanctus <rogersanctus@gmail.com>
2023-01-09 21:32:27 -0300

    feat: validate all the Notas and throw an error if there is any

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { initDB } from './utils/dbReader'
 import { Pedido } from './models/pedido'
-import { Nota } from './models/nota'
+import { Nota, validateNotasItems } from './models/nota'
 import groupBy from 'lodash/groupBy'
 import { processDB } from './utils/dbProcessor'
 import { makeNotasPedidos } from './services/notasPedidos'
@@ -10,6 +10,12 @@ initDB('./assets/db').then((db) => {
   db = processDB(db)
   const pedidos = db.Pedidos as Pedido[]
   const notas = db.Notas as Nota[]
+
+  const notasItemsValidation = validateNotasItems(notas, pedidos)
+
+  if (notasItemsValidation instanceof Error) {
+    throw notasItemsValidation
+  }
 
   const groupedPedidos = groupBy(pedidos, 'id')
   const notasPedidos = makeNotasPedidos(groupedPedidos, notas)
